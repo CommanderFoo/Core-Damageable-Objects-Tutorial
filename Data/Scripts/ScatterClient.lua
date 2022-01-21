69dce55d9cc1cbeb25c0b270b947f49560a6faa5
CommanderFoo <pixeldepth@gmail.com>
2022-01-21 14:59:29 +0000

    Update ScatterClient.lua

--- Data/Scripts/ScatterClient.lua
+++ Data/Scripts/ScatterClient.lua
@@ -4,10 +4,9 @@ local NOT_SUPPORTED = script:GetCustomProperty("NotSupported"):WaitForObject()
 NOT_SUPPORTED:Destroy()
 
 local meshes = SUPPORTED:FindDescendantsByType("StaticMesh")
-local power = 1400
+local power = 400
 local RNG = RandomStream.New()
 
-print(RNG:GetVector3FromCone(Vector3.UP, 190, 190))
 for _, mesh in ipairs(meshes) do
 	mesh.collision = Collision.FORCE_ON
 	mesh.cameraCollision = Collision.FORCE_OFF
@@ -15,7 +14,7 @@ for _, mesh in ipairs(meshes) do
 
 	if mesh.isSimulatingDebrisPhysics then
 		mesh.lifeSpan = RNG:GetNumber(2.5, 5)
-		mesh:SetVelocity(RNG:GetVector3FromCone(Vector3.UP, 0, 0) * power)
+		mesh:SetVelocity(RNG:GetVector3FromCone(Vector3.UP, 90, 90) * power)
 	else
 		mesh:Destroy()
 	end
